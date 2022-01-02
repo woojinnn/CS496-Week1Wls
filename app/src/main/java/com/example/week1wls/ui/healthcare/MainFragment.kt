16013d6dc601b111484f612291e444493c6f8350
woojin <wjl0209@kaist.ac.kr>
2022-01-02 22:54:37 +0900

    implemetned pie chart

--- app/src/main/java/com/example/week1wls/ui/healthcare/MainFragment.kt
+++ app/src/main/java/com/example/week1wls/ui/healthcare/MainFragment.kt
@@ -2,20 +2,22 @@ package com.example.week1wls.ui.healthcare
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.week1wls.R
-import com.example.week1wls.ui.Gallery.GalleryAdapter
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import kotlinx.android.synthetic.main.fragment_gallery.*
 import kotlinx.android.synthetic.main.fragment_healthcare_main.*
 
 //import com.github.mikephil.charting.charts.PieChart
@@ -26,7 +28,7 @@ class MainFragment: Fragment() {
     private lateinit var profileCache: SharedPreferences
     private lateinit var gson: Gson
     private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<HealthAdapter.ViewHolder>? = null
+//    private var adapter: RecyclerView.Adapter<HealthAdapter.ViewHolder>? = null
     lateinit var input : String
 
 
@@ -35,13 +37,13 @@ class MainFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        profileCache = requireActivity().getSharedPreferences("profileCache",
+        profileCache = requireActivity().getSharedPreferences(
+            "profileCache",
             Context.MODE_PRIVATE
         )
         gson = GsonBuilder().create()
 
-        val view = inflater.inflate(R.layout.fragment_healthcare_main, container, false)
-        return view
+        return inflater.inflate(R.layout.fragment_healthcare_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,15 +62,40 @@ class MainFragment: Fragment() {
             tv_agesex.text = "(여) " + profile.age + "세"
         tv_physical.text = profile.height.toString() + " (cm), " + profile.weight.toString() + " (kg)"
 
-        //var pieChart: PieChart = findViewById(R.id.pieChart)
+        // Pie chart
+        pieChart.setUsePercentValues(true)
 
-        /* input 받기 */
-        // search 버튼 눌렸을 때
-        fdsrhBtn.setOnClickListener {
-            input = inputFood.text.toString()
-            inputText.text = null
-            findFood(input)
-        }
+        val pieEntries = ArrayList<PieEntry>()
+        pieEntries.add(PieEntry(profile.total.toFloat(),"Current"))
+        pieEntries.add(PieEntry((profile.bmr - profile.total).toFloat(), "Total")) // Remaining
+
+        val pieDataset = PieDataSet(pieEntries, "")
+        val pieData = PieData(pieDataset)
+        pieDataset.setColors(ColorTemplate.JOYFUL_COLORS, 100)
+        pieDataset.setDrawValues(false)
+        pieDataset.sliceSpace = 2f
+
+        pieChart.legend.isEnabled = false
+        pieChart.description.isEnabled = false
+        pieChart.setDrawEntryLabels(false)
+        pieChart.centerText = "${profile.total} / ${profile.bmr}"
+        pieChart.setCenterTextColor(Color.BLACK)
+        pieChart.setCenterTextSize(10f)
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+        pieChart.data = pieData
+        pieChart.animate()
+        pieChart.invalidate()
+
+        // Edit profile button
+
+
+//        /* input 받기 */
+//        // search 버튼 눌렸을 때
+//        fdsrhBtn.setOnClickListener {
+//            input = inputFood.text.toString()
+//            inputText.text = null
+//            findFood(input)
+//        }
 
     }
 
@@ -76,11 +103,11 @@ class MainFragment: Fragment() {
         super.onDestroyView()
     }
 
-    fun findFood(input: String){
-        // linear layout
-        foodList.apply{
-            layoutManager = LinearLayoutManager(activity)
-            adapter = HealthAdapter()
-        }
-    }
+//    fun findFood(input: String){
+//        // linear layout
+//        foodList.apply{
+//            layoutManager = LinearLayoutManager(activity)
+//            adapter = HealthAdapter()
+//        }
+//    }
 }
